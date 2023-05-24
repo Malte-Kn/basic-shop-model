6627ce397873b0ec20d760230db2c34d65a5edda
Malte <95937698+Malte-Kn@users.noreply.github.com>
2023-05-24 19:05:08 +0200

    Opentimes and other frontend stuff

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { ProductsService } from './products.service';
 import {Subscription} from 'rxjs';
 import { Router } from '@angular/router';
-import { Product } from 'basic-shop-backend/src/products/products.service';
+import { Opentimes, Product } from 'basic-shop-backend/src/products/products.service';
 import { ManagerComponent } from '../manager/manager.component';
 @Component({
   selector: 'app-products',
@@ -14,7 +14,7 @@ export class ProductsComponent implements OnInit {
   title: string = '';
   logo: string = '';
   kategorien: string[]= [];
-  opentimes : any;
+  opentimes : Opentimes ={monday:"9am - 5pm",tuesday:"9am - 15pm",wednesday:"9:00 - Do 1:00",thursday: "9:00 - 17:00",friday: "8:00 - 17:00",saturday: "9:00 - 17:00", sunday:"9:00 - 17:00"};
   subscription = new Subscription();
   public products$: Observable<any> | undefined;
   constructor(private productsService: ProductsService) { }
@@ -32,6 +32,9 @@ changeKat(kategorie: string){
     this.getProducts();
     this.productsService.title.subscribe((response) =>{
       this.title = response.title;
+    });
+    this.productsService.opentimes.subscribe((res: Opentimes) =>{
+      this.opentimes = res;
     });
     this.logo= this.productsService.logo;
     this.kategorien = this.productsService.kategorien;
