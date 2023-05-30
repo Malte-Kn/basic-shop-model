ed06923ff6319f82cbfa9581ed4851bba54b5798
Malte <95937698+Malte-Kn@users.noreply.github.com>
2023-05-30 15:13:11 +0200

    Some Open/Closed options for date / time

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,6 +5,7 @@ import {Subscription} from 'rxjs';
 import { Router } from '@angular/router';
 import { Opentimes, Product } from 'basic-shop-backend/src/products/products.service';
 import { ManagerComponent } from '../manager/manager.component';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -20,15 +21,36 @@ export class ProductsComponent implements OnInit {
   subscription = new Subscription();
   public products$: Observable<any> | undefined;
   constructor(private productsService: ProductsService) { }
-
-
+  date = new Date();
+  hour= this.date.getHours();
+  min = this.date.getMinutes();
+  day = this.date.getDay();
   //this.productsService.addProducts(testproduct).subscribe(product => this.products.push(product));
-
+  open = true;
 public getProducts(){
  this.products$ = this.productsService.getProducts();
 }
 changeKat(kategorie: string){
 
+}
+isOpen(){
+  switch(this.day){
+    case 0:
+      return this.hour > 9
+    case 1:
+      return this.hour > 16
+    case 2:
+      return this.hour > 16
+    case 3:
+      return this.hour > 9
+    case 4:
+      return this.hour > 9
+    case 5:
+      return this.hour > 9
+    case 6:
+      return this.hour > 9
+   }
+  return true
 }
   ngOnInit(): void{
     this.getProducts();
@@ -45,5 +67,7 @@ changeKat(kategorie: string){
     this.logo= this.productsService.logo;
     this.kategorien = this.productsService.kategorien;
     this.opentimes = this.productsService.opentimes
+    console.log(this.hour+"," + this.day)
   }
+
 }
